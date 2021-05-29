40e6143a2c205ab9d5ae8e6220b2ae76a27a0482
Szymulcio <szymonwojnarowski@outlook.com>
2021-05-30 00:07:09 +0200

    Add Javascript

---
+++ shared.js
@@ -0,0 +1,30 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    })
+}
+
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+}
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
